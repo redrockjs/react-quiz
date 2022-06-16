920e0b420251747c851d1dd2f1db898751fdcd74
redrockjs <smb.sochi@gmail.com>
2022-06-16 10:05:10 +0300

    fix: add html to Main

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
-import data from './asssets/data/data.json';
 import {Main} from "./pages/Main";
 import {Header} from "./components/Header";
 import {Result} from "./pages/Result";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {useEffect, useState} from "react";
 
 export default function App() {
-
-  const [state, setState] = useState(null);
-  // const [questionNumber, setQuestionNumber] = useState(1);
-
-
-  useEffect(() => {
-    //console.log(state)
-    setState(data);
-  })
 
   return (
     <BrowserRouter>
       <Header/>
       <Routes>
-        <Route exact path='/' element={<Main/>} state={state}/>
+        <Route exact path='/' element={<Main/>}/>
         <Route exact path='/result' element={<Result/>}/>
       </Routes>
     </BrowserRouter>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,38 @@
-//import {useEffect} from "react";
+import styles from "./Main.module.css"
+import data from "../asssets/data/data.json";
+import {useState} from "react";
 
-export const Main = ({state}) => {
+export const Main = (props) => {
 
-  console.log(state)
+  const [questionNumber, setQuestionNumber] = useState(1);
+
+  const resultArr = [];
+
+  const handleNextBtn = () => {
+    if (questionNumber < 10) {
+      setQuestionNumber(questionNumber + 1)
+    } else {
+      alert("Вопросы закончились");
+    }
+  }
 
   return (
-    <main>Main page</main>
+    <main>
+      <h2 className={styles.main__heading}>Вопросы</h2>
+      <p className={styles.main__question}>
+        {questionNumber + '. ' + data.questions[questionNumber].question}
+      </p>
+      <h2 className={styles.main__heading}>Ответы</h2>
+      <ul className={styles.main__answers}>
+        <li className={styles.main__item}>{data.questions[questionNumber].answers[0]} </li>
+        <li className={styles.main__item}>{data.questions[questionNumber].answers[1]} </li>
+        <li className={styles.main__item}>{data.questions[questionNumber].answers[2]} </li>
+        <li className={styles.main__item}>{data.questions[questionNumber].answers[3]} </li>
+      </ul>
+      <div className={styles.main__buttons}>
+        <button className={styles.main__btn} onClick={handleNextBtn}>Продолжить
+        </button>
+      </div>
+    </main>
   );
 }
