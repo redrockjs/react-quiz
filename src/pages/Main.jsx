f1259256fe90f1934aae8e5475d581d716eb4ae5
redrockjs <smb.sochi@gmail.com>
2022-06-17 11:57:09 +0300

    fix: correct mark of list item

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,12 +5,11 @@ import React, {useState} from 'react';
 export const Main = (props) => {
 
   const [currentAnswer, setCurrentAnswer] = useState(null);
-
   const [questionNumber, setQuestionNumber] = React.useState(1);
 
   const handleNextBtn = () => {
     if (questionNumber < 10) {
-      setQuestionNumber(questionNumber + 1)
+      setQuestionNumber(questionNumber+1)
       setCurrentAnswer(null);
     } else {
       alert("Вопросы закончились");
@@ -18,12 +17,10 @@ export const Main = (props) => {
   }
 
   const handleAnswerItem = (e) => {
-    // TODO: fix key and answer number, 0..3 != 1..4 ))
-    console.log(e.target.dataset.key+' '+data.questions[questionNumber].answer);
-    if (e.target.dataset.key === data.questions[questionNumber].answer) {
-      console.log("Yeaaa!!! Right!!!");
-      setCurrentAnswer(e.target.dataset.key);
-    }
+    let targetAns = +e.target.dataset.key;
+    let currentAns = +data.questions[questionNumber].answer-1;
+
+    if (targetAns === currentAns) setCurrentAnswer(targetAns);
   }
 
   return (
@@ -38,7 +35,7 @@ export const Main = (props) => {
           data.questions[questionNumber].answers.map((el, idx) => {
             return (
               <React.Fragment key={idx}>
-                <li className={!!currentAnswer ? styles.main__item_active : styles.main__item}>
+                <li className={currentAnswer===idx ? styles.main__item_active : styles.main__item}>
                   <span className={styles.main__item_hover} data-key={idx} onClick={handleAnswerItem}> {el} </span>
                 </li>
               </React.Fragment>
