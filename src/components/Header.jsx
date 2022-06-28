6b976b7db9ce6df93188de63f349c06a7543465f
Valeriy G <68702584+redrockjs@users.noreply.github.com>
2022-06-28 14:47:03 +0300

    Revert "Feature/redux"

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import style from './Header.module.css';
-
-export const Header = () => {
-  return (
-    <header className={style.header}>
-      <h1 className={style.header__heading}>Викторина</h1>
-    </header>
-  )
-}
